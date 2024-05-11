78fd83de5ff24066fc53cd17cfaf6fe320049b22
saikoro <135479787+auxces@users.noreply.github.com>
2024-05-10 21:32:18 -0400

    base

--- shine.js
+++ shine.js
@@ -1,48 +1,17 @@
-let x = document.getElementById("shine");
-let y = document.getElementById("peace");
+let x = document.getElementById("background");
 let z = 0;
 
-Window.onload = load();
-function load() {
-  playAudio();
+
+function backgroundSwitch(){
+  if (z==0){
+    x.src="images/clouds.png";
+    z = 1;
+  }
+  else{
+    x.src="images/cloud.png";
+    z = 0;
+  }
+
 }
-
-function playAudio() {
-    x.volume = 0.1;
-    y.volume = 0.1;
-    x.play();
-    y.play();
-  }
-  
-  function pauseAudio() {
-    if (z == 0){
-      x.pause();
-      y.pause();
-      z = 1;
-    }
-    else{
-      x.play();
-      y.play();
-      z = 0;
-    }
-    
-  }
-
-  function muteAudio(){
-    if (x.volume == 0.1){
-      x.volume = 0;
-    } else {
-      x.volume = 0.1;
-    }
-  }
-
-  function muteVideo(){
-    if (y.volume == 0.1){
-      y.volume = 0;
-    } else {
-      y.volume = 0.1;
-    }
-  }
-  
   //disable right click just cause y not 
- // document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
